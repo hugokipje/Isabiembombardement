382944c08fcb2627ca7f25c1197a66e0a00af36b
Hugo Jongeneelen <110241@st-maartenscollege.nl>
2021-02-10 15:17:29 +0000

    knopje verdwijnt bij level 1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var bg;
 var mode;
 let button;
+let bestaatNogNietAl = true;
+let button_lvl1;
 
 function preload(){
     bg = loadImage("https://images3.persgroep.net/rcs/pDCTCuxNq3RMGE1bspT-zbQM2e8/diocontent/70425715/_crop/1/37/856/484/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8");
@@ -12,8 +14,10 @@ function setup() {
   createCanvas(800,800);
 }
 
+
 function draw() {
   clear();
+
   if (mode==0) {
     background(plaatje1);
     text('Isa-biem-bombardement',400,300);
@@ -23,15 +27,21 @@ function draw() {
     textSize(50);
     textFont("Times");
   }
-  if (mode==1) {
+
+  else if (mode==1) {
+    
     background(51);
-    button_lvl1 = createButton('Level 1');
-    button_lvl1.size(200,100);
-    button_lvl1.position(200,400);
-    button_lvl1.mousePressed(lvl1);
+    if(bestaatNogNietAl){
+      button_lvl1 = createButton('Level 1');
+      button_lvl1.size(200,100);
+      button_lvl1.position(200,400);
+      button_lvl1.mousePressed(lvl1);
+      bestaatNogNietAl = false;
+    }
   }
-  if (mode==2){
-    background(bg)
+  else if (mode==2){
+    removeElements(button_lvl1);    
+    background(bg);
   }
 }
 
@@ -42,7 +52,6 @@ function mousePressed() {
 }
 
 function lvl1(){
-  clearCanvas
   mode=2;
 }
 
